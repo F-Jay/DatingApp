8a6ca0dc51cdcdde08e3aeb17f08cfdc9684870c
F-Jay <fscholtz65@gmail.com>
2021-10-12 11:21:56 +0200

    End of Section 10

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 using API.Data;
 using API.DTOs;
@@ -36,6 +37,21 @@ namespace API.Controllers
         public async Task<ActionResult<MemberDto>> GetUser(string username)
         {
             return await _userRepository.GetMemberAsync(username);
+        }
+
+        [HttpPut]
+        public async Task<ActionResult> UpdateUser(MemberUpdateDto memberUpdateDto){
+            var username = User.FindFirst(ClaimTypes.NameIdentifier)?.Value; // Claims - Get Username from token that API uses to Authenticate user.
+            var user = await _userRepository.GetUserByUserNameAsync(username);
+
+            _mapper.Map(memberUpdateDto, user);
+
+            _userRepository.Update(user); // Now user object is flagged by Enity Framwork as Updated.
+
+            if(await _userRepository.SaveAllAsync())
+                           return NoContent();
+            
+            return BadRequest("Failed to update user.");
         }
     }
 }
